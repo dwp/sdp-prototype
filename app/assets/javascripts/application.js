f82166952cb73bb09f992d8145e503d8179d7d9f
Sam <sameastwood.digitaldwp@gmail.com>
2018-12-11 13:53:02 +0000

    monthly payments task history accordians

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,49 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+document.querySelectorAll('.accordian-section').forEach(function(element) {
+  element.addEventListener('click', revealAccordian);
+});
+
+document.querySelectorAll('#accordianOpenAll').forEach(function(element) {
+  element.addEventListener('click', accordianOpenAll);
+});
+
+function revealAccordian(e) {
+  if(!document.getElementById(this.dataset.target).classList.contains('accordian-hidden')) {
+    document.getElementById(this.dataset.target).classList.add('accordian-hidden');
+    switchAccordianImages();
+    return;
+  }
+
+  document.querySelectorAll('.accordian-section').forEach(function(element) {
+    document.getElementById(element.dataset.target).classList.add('accordian-hidden');
+  });
+  document.getElementById(this.dataset.target).classList.remove('accordian-hidden');
+  switchAccordianImages();
+}
+
+function accordianOpenAll(e) {
+  if(this.innerText === 'Open all') {
+    this.innerText = 'Close all';
+    document.querySelectorAll('.accordian-section').forEach(function(element) {
+      document.getElementById(element.dataset.target).classList.remove('accordian-hidden');
+    });
+  } else {
+    this.innerText = 'Open all';
+    document.querySelectorAll('.accordian-section').forEach(function(element) {
+      document.getElementById(element.dataset.target).classList.add('accordian-hidden');
+    });
+  }
+}
+
+function switchAccordianImages() {
+  document.querySelectorAll('.accordian-section').forEach(function(element) {
+    if(document.getElementById(element.dataset.target).classList.contains('accordian-hidden')) {
+      document.getElementById(element.dataset.button).style.backgroundImage = 'url(/public/images/accordian-open.svg';
+    } else {
+      document.getElementById(element.dataset.button).style.backgroundImage = 'url(/public/images/accordian-close.svg)';
+    }
+  });
+}
